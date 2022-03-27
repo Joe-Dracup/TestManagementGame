eabe3524945e2a90c85a6e5e154d1355c0d61b9d
Joe Dracup <joe@dracup.me.uk>
2022-03-27 20:20:44 +0100

    Added missing bracket

--- TestManagementGame/TestManagementGame/ScreenManagement/Screens/SplashScreen.cs
+++ TestManagementGame/TestManagementGame/ScreenManagement/Screens/SplashScreen.cs
@@ -29,7 +29,7 @@ namespace TestManagementGame.ScreenManagement.Screens
             if (title == null)
                 title = new Text2D("Font1", titlePos, "This is a splash screen");
             if (bottomText == null)
-                bottomText = new Text2D("Font1", new Vector2(Globals.Width / 2, Globals.Height - 150, "");
+                bottomText = new Text2D("Font1", new Vector2(Globals.Width / 2, Globals.Height - 150), "");
             if (logo == null)
                 logo = new SplashLogo(new Vector2(Globals.Width / 2, Globals.Height), new Vector2(logoHeight, logoHeight));
 
